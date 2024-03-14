5ca176acb66c3b7224bcd394004303f8bd7b3e9e
Corey Lionis <corey1057105@gmail.com>
2024-03-14 21:39:08 +1100

    Added rules for time steps, and some minor modifications towards visualising the simulation.

--- basic_sandpile.py
+++ basic_sandpile.py
@@ -1,4 +1,8 @@
-import random as rand 
+import random as rand
+import os
+from multiprocessing import Process
+import keyboard
+import time
 
 rand.seed()
 
@@ -15,13 +19,28 @@ def generate_sandpile(t: tuple):
     initial = rand.choices([0,1], k=t[0]*t[1])
     return initial 
 
-def sandpile_step():
+def sandpile_step(sandpile: list[list[int]]):
     '''
     Updates sandpile according to rules.
     '''
-    pass 
+    k0 = rand.randint(0, len(sandpile))
+    k1 = rand.randint(0, len(sandpile[0])
+    sandpile[k0][k1] += 1
+    for i in range(len(sandpile)):
+        for j in range(len(sandpile)):
+            if sandpile[i][j] >= 4:
+                sandpile[i][j] -= 4
+                if i + 1 < len(lst): 
+                    sandpile[i+1][j] = sandpile[i+1][j] + 1
+                if i-1 > -1: 
+                    sandpile[i-1][j] = sandpile[i-1][j] + 1
+                if j + 1 < len(lst[0]):
+                    sandpile[i][j+1] = sandpile[i][j+1] + 1
+                if j - 1 > -1:
+                    sandpile[i][j-1] = sandpile[i][j-1] + 1
+    return sandpile
 
-def visualise_sandpile():
+def sandpile_process(iterations:int = 1000):
     '''
     I don't think this will fit in a single function, but I want
     to display sandpile evolution on a grid that continuously updates. 
@@ -34,12 +53,22 @@ def visualise_sandpile():
     ~pretty~, maybe some colour presets would be nice also. Don't 
     get too bogged down in cosmetics though. 
     '''
-    pass 
+    pass
+
+
+
+if __name__ == '__main__':
+    process = Process(target=visualise_sandpile)
+    process.start()
+    while process.is_alive():
+        if keyboard.is_pressed('q'):
+            process.terminate()
+            break
 
 class Sandpile: 
     '''
     My instincts say we should wrap this all in an object. 
     '''
-    pass
+    def __init__(self, dimensions, palette):
 
 print(generate_sandpile((8,9)))
