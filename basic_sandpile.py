4d0c6008c9cca0f4f87e612941709b14f00fb042
Corlio5994 <corey1057105@gmail.com>
2024-05-25 21:32:17 +1000

    Added skeleton for stats tracking, refined sandpile generation to enable multiple piles of sand in different places, wrote a render function as alternative to saving to PDF

--- basic_sandpile.py
+++ basic_sandpile.py
@@ -5,25 +5,64 @@ import matplotlib.pyplot as plt
 import time
 import math
 import pickle
+import numpy as np
 from datetime import datetime
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.patches as patches
 
 rand.seed()
 
-def generate_sandpile(t: tuple):
+
+#IDEA : Larger grid for sandpile size. Changes to gen are : take in grid dimensions, as well as site dimensions. To further generalise, we can allow multiple sites. This is simple - supply a matrix of site
+# sizes. Either generate random locations or manually input
+
+#Note: It is allowed for users to input overlap. This will just mean that the intersection of sites gets rewritten, which might be inefficient but should be ok since the work done should be that of the average case. 
+
+
+
+def generate_sandpile(grid: tuple, sites, locations=False):
     '''
     Function for generating a sandpile. Initialises 2D-array with
     t[0] rows and t[1] columns, with 1's representing grains of sand.
+    The user specifies the sizes and optionally the locations of 
+    regions with grains of sand.
+    Examples: 
+        generate_sandpile((x,y), [(x,y)]) will cover the whole array with sand grains. 
+        generate_sandpile((x,x), [(x-2a,x-2a), (a-1, a-1)]) will create a square with side length x-2a at the centre of the grid.
     :input: A tuple t = (number of rows, number of columns) specifying
             size of sandpile simulation. 
     :output: A 2D-array of binary digits, with 1 representing a grain
             of sand and 0 representing an empty space.
-    :complexity: O(random.choices)
-    '''
-    initial = []
-    for _ in range(t[0]):
-        initial.append(rand.choices([0,1], k=t[1]))
-    return initial 
+    '''   
+    table = np.zeros(grid)
+    size_sum = 0 
+    if locations == False:
+        for s in sites: 
+            size_sum += s[0]*s[1]
+    else: 
+        for s in sites: 
+            size_sum += s[0][0]*s[0][1]
+
+    if size_sum > grid[0]*grid[1]:
+        raise ValueError(f"Total site dimensions are {size_sum}, input smaller sites.")
+    #Format of each s: [(length, width), (location_x, location_y)] if locations = True, (length,width) otherwise 
+    if locations == True:
+        for s in sites: 
+            for i in range(s[1][0], min(s[1][0]+s[0][0]-1, len(table))):
+                for j in range(s[1][1], min(s[1][1]+s[0][1]-1, len(table[0]))):
+                        table[i][j] = rand.choice([0,1])
+    else:
+        head = [0,0] 
+        for s in sites: 
+            for i in range(head[0], min(head[0]+s[0]-1, len(table))):
+                for j in range(head[1], min(head[1]+s[1]-1, len(table[0]))):
+                        table[i][j] = rand.choice([0,1])
+            head = [head[0]+s[0]-1, head[1]+s[1]-1]
+            if head[0] > len(table):
+                head[0] = 0
+            if head[1] > len(table[1]):
+                head[1] = 0       
+    return table
 
 def sandpile_step(sandpile: list[list[int]]):
     '''
@@ -59,6 +98,20 @@ def sandpile_process(sandpile: list[list[int]], visual:bool = True, iterations:i
     ~pretty~, maybe some colour presets would be nice also. Don't 
     get too bogged down in cosmetics though. 
     '''
+    def avg_height(sandpile: list[list[int]]) -> float:
+        h = 0 
+        for i in range(len(sandpile)): 
+            for j in range(len(sandpile[i])):
+                h += sandpile[i][j]
+        return h/(len(sandpile)*len(sandpile[0]))
+    def loss():
+        pass
+    def area():
+        pass 
+    def topples():
+        pass
+    def length(): 
+        pass 
     snapshots = math.ceil((len(sandpile)*len(sandpile[0])*iterations)/10**7)
     step = 0
     counter = 0 
@@ -66,28 +119,52 @@ def sandpile_process(sandpile: list[list[int]], visual:bool = True, iterations:i
     norm = plt.Normalize(vmin = 0, vmax = 4)
     #TODO: Rename snaps
     snaps = []
+    with open(f'logstats-{datetime.now().strftime("%m-%d-%H")}.txt', 'a') as f:
+        f.write(f'Simulation starting at {datetime.now().strftime("%m-%d-%H-%M")}. \n')
     while step < iterations: 
+        #if counter == snapshots-1:
+            #past = sandpile
         sandpile = sandpile_step(sandpile)
         counter+=1
-        # take pictures of the sandpile every f steps,
+        # take pictures and record statistics of the sandpile every f steps,
         # where f is chosen so that the number of snapshots
         # taken has storage size <= ~20mb
         if counter == snapshots:
             print(f'Up to step {step}.')
-        if counter == snapshots and visual== True:
-            snaps.append(pickle.dumps(cmap(norm(sandpile))))
-            counter = 0
+            with open(f'logstats-{datetime.now().strftime("%m-%d-%H")}.txt', 'a') as f:
+                f.write(f'Average height at step {step} is: {avg_height(sandpile)} \n')
+            if visual== True:
+                snaps.append(pickle.dumps(cmap(norm(sandpile))))
+                counter = 0
         step += 1
+    with open(f'logstats-{datetime.now().strftime("%m-%d-%H")}.txt', 'a') as f:
+        f.write('--------------------------------------\n')
     if visual == True: 
         print('Unpickling snapshots...')
-        time = datetime.now().strftime("%H-%M-%S")
-        fname = 'RecordedSimSnapshots_' + time
+        time = datetime.now().strftime("%m-%d-%H-%M")
+        fname = 'RecordedSimSnapshots_' + time + '.pdf'
         f = PdfPages(fname)
         for i in range(len(snaps)): 
             pic = pickle.loads(snaps[i])
             plt.imsave(f, pic, format='pdf')
         f.close()
     return sandpile
+
+def render_sandpile(sandpile: list[list[int]]):
+
+    # plot
+    fig, ax = plt.subplots()
+    ax.set_axis_off()
+    im = ax.imshow(sandpile)
+
+    # create legend
+    values = np.unique(sandpile.ravel())
+    colours = [im.cmap(im.norm(x)) for x in values]
+    cutouts = [patches.Patch(color=colours[i], label=f'{i}') for i in range(len(colours))]
+    plt.legend(handles=cutouts, bbox_to_anchor=(1.05,1), loc=2, title='Pile height', alignment='left', edgecolor='black')
+
+    plt.show()
+    return None
 
 
 
@@ -105,5 +182,6 @@ def sandpile_process(sandpile: list[list[int]], visual:bool = True, iterations:i
 #    '''
 #    def __init__(self, dimensions, palette):
 
-g = generate_sandpile((8,9))
-sandpile_process(g, iterations = 10000)
+g = generate_sandpile((90,90), [[(50,50), (19,19)]], locations=True)
+render_sandpile(g)
+#sandpile_process(g, visual=True, iterations=40000)
